fbed0069f9ed5af1ecc18e992f4803233e407dba
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-18 14:40:35 +0100

    [UPD] sistemato l`errore nella verifica dei numeri divisibili sia per 3 che per 5

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
 for (let i = 0; i<100; i++){
     // verifica numeri divisibili per 3
     if((i+1) % 3 == 0) {
-        console.log('Fizz')
         // verifica numeri divisibili sia per 3 che per 5
-        if((i+1) % 5 == 0){
+        if ((i+1) % 5 == 0){
             console.log('FizzBuzz')
         }
+        else console.log('Fizz')
     }
     // verifica numeri divisibili per 5
     else if ((i+1) % 5 == 0){
@@ -15,5 +15,5 @@ for (let i = 0; i<100; i++){
     }
 
     // i numeri restanti
-    console.log(i + 1)
+    else console.log(i + 1)
 }
