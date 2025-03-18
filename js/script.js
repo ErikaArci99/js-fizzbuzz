55d87132c6a56134096fed8348ef257da1ed3c7d
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-18 14:24:07 +0100

    [UPD] aggiunta verifica numeri divisibili per 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 // ciclo che permette di stampare i numeri da 1 a 100
 for (let i = 0; i<100; i++){
-
+    // verifica numeri divisibili per 3
+    if(i % 3 == 0) {
+        console.log('Fizz')
+    }
     console.log(i + 1)
 }
